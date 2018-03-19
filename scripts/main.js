bc4f29e2c7fdab9ab9bb42ba8fe7fe0aba4c9c1c
stefanamurarasu <murarasu.stefana@gmail.com>
2018-03-19 11:33:40 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/*eslint no-undef: "error"*/
+/*eslint-env browser*/
+
+let isMenuOpen = false;
+
+function toggleNav() {
+    "use strict";
+    
+	if(isMenuOpen === false) { 
+		document.getElementById("navMenu").style.width = "250px";
+		document.getElementById("header").style.marginLeft = "250px";
+		document.getElementById("content").style.marginLeft = "250px";
+		document.getElementById("footer").style.marginLeft = "250px";
+		isMenuOpen = true;
+	} else {
+		document.getElementById("navMenu").style.width = "0";
+		document.getElementById("header").style.marginLeft = "0";
+		document.getElementById("content").style.marginLeft = "0";
+		document.getElementById("footer").style.marginLeft = "0";
+		isMenuOpen = false;
+	}
+}
