e67e7c819d2662f92e51892bf0ae1c4bf9cc101b
Codruta <andreeacodruta89@yahoo.com>
2018-03-30 12:28:42 +0300

    Adaugat modificari

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,18 @@ let isMenuOpen = false;
 
 function toggleNav() {
     "use strict";
-    
-	if(isMenuOpen === false) { 
-		document.getElementById("navMenu").style.width = "250px";
-		document.getElementById("header").style.marginLeft = "250px";
-		document.getElementById("content").style.marginLeft = "250px";
-		document.getElementById("footer").style.marginLeft = "250px";
-		isMenuOpen = true;
-	} else {
-		document.getElementById("navMenu").style.width = "0";
-		document.getElementById("header").style.marginLeft = "0";
-		document.getElementById("content").style.marginLeft = "0";
-		document.getElementById("footer").style.marginLeft = "0";
-		isMenuOpen = false;
-	}
+
+    if (isMenuOpen === false) {
+        document.getElementById("navMenu").style.width = "320px";
+        document.getElementById("header").style.marginLeft = "320px";
+        document.getElementById("content").style.marginLeft = "320px";
+        document.getElementById("footer").style.marginLeft = "320px";
+        isMenuOpen = true;
+    } else {
+        document.getElementById("navMenu").style.width = "0";
+        document.getElementById("header").style.marginLeft = "0";
+        document.getElementById("content").style.marginLeft = "0";
+        document.getElementById("footer").style.marginLeft = "0";
+        isMenuOpen = false;
+    }
 }
