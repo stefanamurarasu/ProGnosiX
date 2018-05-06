3d623d5d5febf994b4160d998bc0e749382339a0
Codruta <andreeacodruta89@yahoo.com>
2018-05-06 22:24:36 +0300

    modified page

--- public/sidemenu.js
+++ public/sidemenu.js
@@ -1,22 +1,16 @@
 let isMenuOpen = false;
 
 function toggleNav() {
-    "use strict";
 
-    if (isMenuOpen === false) {
+    if (isMenuOpen == false) {
         document.getElementById("navMenu").style.width = "320px";
-        document.getElementById("header").style.marginLeft = "320px";
-        document.getElementById("content").style.marginLeft = "320px";
-        document.getElementById("timeline").style.marginLeft = "320px";
-        document.getElementById("footer").style.marginLeft = "320px";
+		document.body.style.backgroundColor = "rgba(0,0,0,1)";
         
         isMenuOpen = true;
+		
     } else {
         document.getElementById("navMenu").style.width = "0";
-        document.getElementById("header").style.marginLeft = "0";
-        document.getElementById("content").style.marginLeft = "0";
-        document.getElementById("timeline").style.marginLeft = "0";
-        document.getElementById("footer").style.marginLeft = "0";
-        isMenuOpen = false;
+		
+		isMenuOpen = false;
     }
 }
