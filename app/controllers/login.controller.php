a0857c71fb9a3aabd612f027b0d4b70317dab213
Codruta <andreeacodruta89@yahoo.com>
2018-05-22 11:21:01 +0300

    add login controller

--- app/controllers/login.controller.php
+++ app/controllers/login.controller.php
@@ -1,53 +1,33 @@
 <?php
+    session_start();
 
-session_start();
-$_SESSION['message'] = '';
+    switch ($_SERVER['REQUEST_METHOD']){
+        case "GET":
+           if(isset($_SESSION["token"])){
+               header("Location: ../views/home.views.html");
+            } else if(isset($_SESSION["login_failed"])){
+                unset($_SESSION["login_failed"]); 
+                include_once "../views/login_failed.views.html";
+            } else{
+                include_once "../views/login.views.html";
+            }
+            break;
+        case "POST":
+           include_once "../models/auth.model.php";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = 'prognosix';
+           if(isset($_POST["submit_login"])){
+               $loginToken = Auth::login($_POST["username"], $_POST["password"]);
 
-$connection = new mysqli($servername, $username, $password) or die(mysqli_error());
-$select_db = mysqli_select_db($connection, $db) or die("Conectarea la baza de date nu se poate efectua!");
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-    if (isset($_POST["submit_login"])){
-
-        if(!empty($_POST['username']) && !empty($_POST['psw'])){
-
-            $username = $_POST['username'];
-            $psw = $_POST['psw'];
-
-                $_SESSION['username'] = $username;
-
-                $sql = mysqli_query($connection,"SELECT * FROM studenti WHERE psw='".$psw."' AND username='".$username."'");
-
-                $result = mysqli_num_rows($sql);
-
-                if($result != 0){
-                    while($row = mysqli_fetch_assoc($sql)){
-                        $databaseusername=$row['username'];
-                        $databasepassword=$row['psw'];
-                    }
-                    if($username == $databaseusername && $psw == $databasepassword){
-                       ?>
-                        <script>alert("V-ați conectat la contul dumneavoastra cu succes!");</script>
-                       <?php
-                       header("Refresh: 1; url=materii.html");
-                    }
-                }
-                else {
-                       ?>
-                        <script>alert("Ne pare rau, dar parola sau numele de utilizator sunt incorecte! încercați din nou!");</script>
-                       <?php
-                       header("Refresh: 1; url=index.html");
-                }
+               if($loginToken) {
+                $_SESSION["token"] = $loginToken;
+                header("Location: ../views/home.views.html");
+            } else {
+                $_SESSION["login_failed"] = TRUE;
+                header("Location: /");
+            }
         }
-    else{
-        $SESSION['message'] = 'Session Registration Failed!';
-        }   
+        break;
+    default:
+        break;
     }
-}
 ?>
