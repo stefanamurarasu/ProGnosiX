1813c408c898e5b588dc8bc52208b0e21c72c48f
stefanamurarasu <murarasu.stefana@gmail.com>
2018-05-30 16:57:18 +0300

    change condition

--- app/controllers/login.controller.php
+++ app/controllers/login.controller.php
@@ -3,31 +3,36 @@
 
     switch ($_SERVER['REQUEST_METHOD']){
         case "GET":
+
            if(isset($_SESSION["token"])){
                header("Location: ../views/home.view.html");
             } else if(isset($_SESSION["login_failed"])){
                 unset($_SESSION["login_failed"]); 
-                header("Location: /");
-            } else{
+                header("Location: ../views/login_failed.view.html");
+            } else {
                 include_once "../views/login.view.html";
             }
             break;
+
         case "POST":
+
            include_once "../models/auth.model.php";
 
            if(isset($_POST["submit_login"])){
                $loginToken = Auth::login($_POST["username"], $_POST["psw"]);
 
                if($loginToken) {
-                $_SESSION["token"] = $loginToken;
-                header("Location: ../views/home.view.html");
-            } else {
-                $_SESSION["login_failed"] = TRUE;
-                header("Location: /");
+                    $_SESSION["token"] = $loginToken;
+                    header("Location: ../views/home.view.html");
+                } else {
+                    $_SESSION["login_failed"] = TRUE;
+                    header("Location: ../views/login_failed.view.html");
+                }
             }
-        }
-        break;
-    default:
-        break;
+
+            break;
+
+        default:
+            break;
     }
 ?>
