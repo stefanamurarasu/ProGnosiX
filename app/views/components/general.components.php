99491686176333abd778904147cc973b848d1bd5
stefanamurarasu <murarasu.stefana@gmail.com>
2018-05-31 11:57:02 +0300

    add component for course menu

---
+++ app/views/components/general.components.php
@@ -0,0 +1,17 @@
+<?php 
+    class Components {
+        static function createMenu(){
+            return '
+            <div id="navMenu" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="toggleNav()">&times;</a>
+                <a href="./first_course.view.html">Tehnologii Web</a>
+                <a href="./second_course.view.html">Practica SGBD</a>
+                <a href="./third_course.view.html">Programare Avansată</a>
+                <a href="./fourth_course.view.html">Ingineria Programării</a>
+                <a href="./fifth_course.view.html">Criptografie</a>
+                <a href="./sixth_course.view.html">Matlab</a>
+            </div>
+            ';
+        }
+    }
+?>
