3be188c22d86a663e57340cdd7881c08852aea1a
stefanamurarasu <murarasu.stefana@gmail.com>
2018-06-12 04:38:23 +0300

    change typo

--- app/models/results.model.php
+++ app/models/results.model.php
@@ -9,10 +9,10 @@ $conn = new mysqli($GLOBALS["servername"], $GLOBALS["username"], $GLOBALS["passw
 //clasa pentru perioada in care sunt active rundele
 class Result {
 
-    static function insertResults($year, $filepath, $groupNb, $course_id, $description, $eval_type){
+    static function insertResults($year, $filepath, $csv_path, $pdf_path, $groupNb, $course_id, $description, $eval_type){
         global $conn;
 
-        $sql = "INSERT INTO result (course_year, filepath, group_number, course_id, result_description, eval_type) VALUES ('{$year}', '{$filepath}', '{$groupNb}', '{$course_id}', '{$description}', '{$eval_type}')";
+        $sql = "INSERT INTO result (course_year, filepath, csv_path, pdf_path, group_number, course_id, result_description, eval_type) VALUES ('{$year}', '{$filepath}', '{$csv_path}', '{$pdf_path}', '{$groupNb}', '{$course_id}', '{$description}', '{$eval_type}')";
 
         $conn->query($sql);
         return 1;
@@ -24,6 +24,26 @@ class Result {
 
         $result = $conn->query($sql);
         $filepath =  $result -> fetch_assoc()["filepath"];
+
+        return $filepath;
+    }
+
+    static function getCSVPath($year, $course_id, $eval_type){
+        global $conn;
+        $sql = "SELECT csv_path FROM result WHERE course_id = '". $course_id ."' AND course_year = '". $year ."' AND eval_type = '". $eval_type ."'";
+
+        $result = $conn->query($sql);
+        $filepath =  $result -> fetch_assoc()["csv_path"];
+
+        return $filepath;
+    }
+
+    static function getPDFPath($year, $course_id, $eval_type){
+        global $conn;
+        $sql = "SELECT pdf_path FROM result WHERE course_id = '". $course_id ."' AND course_year = '". $year ."' AND eval_type = '". $eval_type ."'";
+
+        $result = $conn->query($sql);
+        $filepath =  $result -> fetch_assoc()["pdf_path"];
 
         return $filepath;
     }
