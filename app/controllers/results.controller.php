5cf98e353964a9f1bb47352c841d05ef9fa914d9
stefanamurarasu <murarasu.stefana@gmail.com>
2018-06-12 04:37:34 +0300

    change arguments

--- app/controllers/results.controller.php
+++ app/controllers/results.controller.php
@@ -1,6 +1,16 @@
 <?php
     session_start();
 
+    require_once "C:/xampp/htdocs/ultima-ultima-versiune/vendor/autoload.php";
+
+    //$name does not exist and $pdf_path is an absolute path that probably does not exist.//
+    use PhpOffice\PhpSpreadsheet\IOFactory;
+    use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
+    use PhpOffice\PhpSpreadsheet\Spreadsheet;
+    use PhpOffice\PhpSpreadsheet\Writer;
+    use PhpOffice\PhpSpreadsheet\Writer\Pdf;
+    require "C:/xampp/htdocs/ultima-ultima-versiune/vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Writer/Pdf/Mpdf.php";
+
     switch($_SERVER['REQUEST_METHOD']){
         case "GET":
             include_once "../models/user.model.php";
@@ -48,26 +58,57 @@
                 $course = isset($_POST['selectCourse']) ? $_POST['selectCourse'] : false;
                 $type = isset($_POST['selectType']) ? $_POST['selectType'] : false;
                 $resultDescription = isset($_POST['result']) ? $_POST['result'] : false;
-                $filepath =  $target_file;
+                
 
-                // Check if file already exists
+                // Fisierul exista deja
                 if (file_exists($target_file)) {
-                    echo "Sorry, file already exists.";
+                    echo "Fisier existent.";
                     $uploadOk = 0;
                 }
 
+                $filepath = $target_file;
 
-                // Check if $uploadOk is set to 0 by an error
                 if ($uploadOk == 0) {
-                    echo "Sorry, your file was not uploaded.";
-                // if everything is ok, try to upload file
+                    echo "Fisierul nu poate fi incarcat!";
+
                 } elseif (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                } else {
-                        echo "Sorry, there was an error uploading your file.";
+                        echo "Fisierul ". basename( $_FILES["fileToUpload"]["name"]). " a fost incarcat cu succes.";
                 }
 
-                $result = Result :: insertResults($year,  $filepath, $groupNb, $course, $resultDescription, $type);
+                // convert to CSV and PDF
+               
+
+                $new_excel_path = "C:/xampp/htdocs/ultima-ultima-versiune/app/controllers/uploads/rezultate.xlsx";
+                $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
+                $spreadsheet = $reader->load("$new_excel_path");
+
+                //echo 'Am ajuns dupa rezultate.xlsx';
+
+                //creeaza writer
+                //$writer = \PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf($spreadsheet); 
+
+                //$class = \PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf::class;
+                //\PhpOffice\PhpSpreadsheet\IOFactory::registerWriter('Pdf', $class);
+                //$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Pdf');
+
+
+                $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Mpdf'); 
+
+                $name="rezultate.pdf";
+                $pdf_path = 'C:/xampp/htdocs/ultima-ultima-versiune/app/controllers/uploads/' . $name ;
+                
+                $writer->save($pdf_path);
+                
+
+                $csv_path = 'C:/xampp/htdocs/ultima-ultima-versiune/app/controllers/uploads/rezultate.csv';
+                $writer2 = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
+                $writer2->save($csv_path);
+                // / convert to CSV and PDF
+
+                // $filepath =  $target_file;
+                $csvPath = 'uploads/rezultate.csv';
+                $pdfPath = 'uploads/rezultate.pdf';
+                $result = Result :: insertResults($year,  $filepath, $csvPath, $pdfPath, $groupNb, $course, $resultDescription, $type);
                 
                 if ($result){
                     $_SESSION["results_worked"] = TRUE;
@@ -75,8 +116,8 @@
                 } else {
                     $_SESSION["results_failed"] = TRUE;
                     header("Location: ../views/round_failed.view.html");
-                }
-           }
+                }    
+            }
 
            if(isset($_POST["logout_user"])) {
                 Auth::logout();
